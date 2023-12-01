0db18a35578b8853d0a605bf29f81dd585bec42a
johnadriellelalongisip <144537154+johnadriellelalongisip@users.noreply.github.com>
2023-12-01 17:13:36 +0800

    12345adrielle

--- PROJECT/application/mainurls.py
+++ PROJECT/application/mainurls.py
@@ -1,13 +1,16 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from .views import login, register, dashboard, form, patient, records, inventory
+from .views import login, register, dashboard, form, patient, records, inventory, appointment
 
 urlpatterns = [
+    path('', dashboard, name='dashboard'),  # Add this line for the empty path
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('dashboard/', dashboard, name='dashboard'),
     path('form/', form, name='form'),
+    path('appointment/',appointment, name='appointment'),
     path('patient/', patient, name='patient'),
     path('records/', records, name='records'),
     path('inventory/', inventory, name='inventory'),
+    
 ]
