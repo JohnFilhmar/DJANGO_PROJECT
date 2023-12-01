cc4cf10be642dd1053f675b0001118230c7ac7e5
johnadriellelalongisip <144537154+johnadriellelalongisip@users.noreply.github.com>
2023-12-01 21:21:46 +0800

    dbsetup
    pre follow mo instructions sa chapter 2 nang lecture ng sir salamat

--- PROJECT/application/mainurls.py
+++ PROJECT/application/mainurls.py
@@ -1,6 +1,6 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from .views import login, register, dashboard, form, patient, records, inventory, appointment
+from .views import login, register, dashboard, form, patient, records, inventory
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),  # Add this line for the empty path
@@ -8,7 +8,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('dashboard/', dashboard, name='dashboard'),
     path('form/', form, name='form'),
-    path('appointment/',appointment, name='appointment'),
     path('patient/', patient, name='patient'),
     path('records/', records, name='records'),
     path('inventory/', inventory, name='inventory'),
--- PROJECT/myproject/settings.py
+++ PROJECT/myproject/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'MHCV',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
+        
+    }
+}
